07591b28a677ddd836f7562f539d46c6bbbe6c38
Aadit Juneja <aadit.juneja12@gmail.com>
2025-06-15 21:10:00 -0700

    just need to load from dram and add a host function

--- fa1_fwd.cu
+++ fa1_fwd.cu
@@ -2,10 +2,10 @@
 #include <mma.h>
 #include <iostream>
 
-#define TILE_X_SIZE 16
-#define TILE_Y_SIZE 8//for non square tiles 
+#define TILE_X_SIZE 8
+#define TILE_Y_SIZE 16//for non square tiles 
 #define SQUARE_TILE_SIZE TILE_X_SIZE//for 16x16 tiles
-#define SHARED_Q_K_DIM TILE_X_SIZE
+#define SHARED_Q_K_DIM TILE_Y_SIZE
 /*
 -TILE_X_1---TILE_X_2---TILE_X_3---TILE_X_4---TILE_X_5---TILE_X_6---TILE_X_7---TILE_X_8-
 ...
@@ -23,50 +23,150 @@ right now T1=float16, T2=float32 always
 #define WARP_SIZE 32
 
 
-template <typename T1, typename T2,int b_c, int qkv_dim>
-__device__ void calcQKT(T1* shared_q, T1* shared_k, int seq_len, int laneid,int warpid) {
-    int req_x_tiles=ceil(b_c/TILE_X_SIZE);
-    int req_y_tiles=ceil(b_r/TILE_Y_SIZE);
-    int req_tiles=req_x_tiles*req_y_tiles;//output qk^t tiles 
-    for (int i=0;i<req_tiles;i+=WARPS_PER_BLOCK) {
-        if (i+warpid<req_tiles) {
-            int x_idx=(i+warpid)%req_x_tiles;
-            int y_idx=(i+warpid)/req_x_tiles;
-            int output_tile_uleft[2]={y_idx*TILE_Y_SIZE,x_idx*TILE_X_SIZE};//upper left's row, col
-            T2 rC[4]={0,0,0,0};
-            for (int j=0;j<qkv_dim/SHARED_Q_K_DIM;j++) {
-                int q_uleft[2]={output_tile_uleft[0],output_tile_uleft[1]+j*SHARED_Q_K_DIM};
-                int k_uleft[2]={output_tile_uleft[1],output_tile_uleft[0]+j*SHARED_Q_K_DIM};//LOAD IN TRANSPOSE!!! 
-                //load to registers, execute ptx oh 
-                T1 q_elements[8]=[
-                    shared_q[q_uleft[0]+laneid/4][q_uleft[1]+2*(laneid%4)],
-                    shared_q[q_uleft[0]+laneid/4][q_uleft[1]+2*(laneid%4)+1],
-                    shared_q[q_uleft[0]+laneid/4+8][q_uleft[1]+2*(laneid%4)],
-                    shared_q[q_uleft[0]+laneid/4+8][q_uleft[1]+2*(laneid%4)+1],
-                    shared_q[q_uleft[0]+laneid/4][q_uleft[1]+8+2*(laneid%4)],
-                    shared_q[q_uleft[0]+laneid/4][q_uleft[1]+8+2*(laneid%4)+1],
-                    shared_q[q_uleft[0]+laneid/4+8][q_uleft[1]+8+2*(laneid%4)],
-                    shared_q[q_uleft[0]+laneid/4+8][q_uleft[1]+8+2*(laneid%4)+1]
-                    ];//thank you to https://veitner.bearblog.dev/ for making the register loading a lot easier
-                    T1 k_elements[4]=[
-                        shared_k[k_uleft[0]+laneid/4][k_uleft[1]+2*(laneid%4)],
-                        shared_k[k_uleft[0]+laneid/4][k_uleft[1]+2*(laneid%4)+1],
-                        shared_k[k_uleft[0]+laneid/4][k_uleft[1]+2*(laneid%4)+8],
-                        shared_k[k_uleft[0]+laneid/4][k_uleft[1]+2*(laneid%4)+9]//handle the transpose here, prone to error section
-                    ];
-                    //use ptx instruction!
-                      asm("mma.sync.aligned.m16n8k16.row.col.f32.f16.f16.f32"//just handling this type pattern for now
-                    "{%0,%1,%2,%3}, {%4,%5,%6,%7}, {%8,%9}, {%10,%11,%12,%13};\n"
-                    : "=f"(rC[0]), "=f"(rC[1]), "=f"(rC[2]), "=f"(rC[3])
-                    : "r"(q_elements[0]), "r"(q_elements[1]), "r"(q_elements[2]), "r"(q_elements[3]), "r"(k_elements[0]), "r"(k_elements[1]),
-                        "f"(rC[0]), "f"(rC[1]), "f"(rC[2]), "f"(rC[3]));
-        }
-        //store to SM 
+template <typename T1, typename T2,int b_c, int b_r, int qkv_dim>
+__device__ void calcQKT(T1* shared_q, T1* shared_k, T2* shared_qkt,int seq_len, int laneid,int warpid) {
+    int req_x_tiles=ceil(b_c/TILE_Y_SIZE);
+    int req_y_tiles=ceil(b_r/TILE_X_SIZE);
+    int req_tiles=req_x_tiles*req_y_tiles;//# of tiles in full qk^t block output
+    for (int i=warpid;i<req_tiles;i+=WARPS_PER_BLOCK) {
+
+        int x_idx=(i)%req_x_tiles;
+        int y_idx=(i)/req_x_tiles;
+        int output_tile_uleft[2]={y_idx*TILE_Y_SIZE,x_idx*TILE_X_SIZE};//upper left's row, col
+        T2 rC[4]={0,0,0,0};
+        for (int j=0;j<qkv_dim/SHARED_Q_K_DIM;j++) {
+            int q_uleft[2]={output_tile_uleft[0],j*SHARED_Q_K_DIM};
+            int k_uleft[2]={output_tile_uleft[1],j*SHARED_Q_K_DIM};//storing transpose directly, row wise traversal for both Q, K tile
+            T1 q_elements[8]=[
+                shared_q[q_uleft[0]+laneid/4][q_uleft[1]+2*(laneid%4)],
+                shared_q[q_uleft[0]+laneid/4][q_uleft[1]+2*(laneid%4)+1],
+                shared_q[q_uleft[0]+laneid/4+8][q_uleft[1]+2*(laneid%4)],
+                shared_q[q_uleft[0]+laneid/4+8][q_uleft[1]+2*(laneid%4)+1],
+                shared_q[q_uleft[0]+laneid/4][q_uleft[1]+8+2*(laneid%4)],
+                shared_q[q_uleft[0]+laneid/4][q_uleft[1]+8+2*(laneid%4)+1],
+                shared_q[q_uleft[0]+laneid/4+8][q_uleft[1]+8+2*(laneid%4)],
+                shared_q[q_uleft[0]+laneid/4+8][q_uleft[1]+8+2*(laneid%4)+1]
+                ];//thank you to https://veitner.bearblog.dev/ for making the register loading a lot easier
+                T1 k_elements[4]=[
+                    shared_k[k_uleft[0]+laneid/4][k_uleft[1]+2*(laneid%4)],
+                    shared_k[k_uleft[0]+laneid/4][k_uleft[1]+2*(laneid%4)+1],
+                    shared_k[k_uleft[0]+laneid/4][k_uleft[1]+2*(laneid%4)+8],
+                    shared_k[k_uleft[0]+laneid/4][k_uleft[1]+2*(laneid%4)+9]//danger
+                ];
+                //use ptx instruction!
+                    asm("mma.sync.aligned.m16n8k16.row.col.f32.f16.f16.f32"//just handling the f32 accum f16 mat A,B pattern for now
+                "{%0,%1,%2,%3}, {%4,%5,%6,%7}, {%8,%9}, {%10,%11,%12,%13};\n"
+                : "=f"(rC[0]), "=f"(rC[1]), "=f"(rC[2]), "=f"(rC[3])
+                : "r"(q_elements[0]), "r"(q_elements[1]), "r"(q_elements[2]), "r"(q_elements[3]), "r"(k_elements[0]), "r"(k_elements[1]),
+                    "f"(rC[0]), "f"(rC[1]), "f"(rC[2]), "f"(rC[3]));
     }
+    //store to smem
+    shared_qkt[output_tile_uleft[0]+laneid/4][output_tile_uleft[1]+2*(laneid%4)]=rC[0];
+    shared_qkt[output_tile_uleft[0]+laneid/4][output_tile_uleft[1]+2*(laneid%4)+1]=rC[1];
+    shared_qkt[output_tile_uleft[0]+laneid/4+8][output_tile_uleft[1]+2*(laneid%4)]=rC[2];
+    shared_qkt[output_tile_uleft[0]+laneid/4+8][output_tile_uleft[1]+2*(laneid%4)+1]=rC[3];
+    }
+}
+
+
+template<typename T1, typename T2, int b_c, int b_r>
+__device__ void reductionStep(T2* shared_qkt, T2* maxValues, T2* sumValues,T2* output, T2* intermediateRowMaxes, T2* intermediatePV, T1* casted_qkt, int warpid, int laneid) {
+    //calculate maxValues, P_{ij} matrix, and l_ij values. split work for each row across warps
+
+    for (int i=warpid;i<b_r;i+=WARPS_PER_BLOCK) {
+        T2 m_ijProposal=-INFINITY;
+        for (int j=laneid;j<b_c;j+=WARP_SIZE) {
+            T2 m_ijProposal=max(m_ijProposal,shared_qkt[i][j]);
+        }
+        for (int offset=WARP_SIZE/2;offset>0;offset>>=1) {
+            m_ijProposal=max(m_ijProposal,__shfl_down_sync(0xFFFFFFFF,m_ijProposal,offset));
+        }
+        if (laneid == 0) {
+            maxValues[i]=max(maxValues[i],m_ijProposal);
+        }
+        m_ijProposal=__shfl_sync(0xFFFFFFFF,m_ijProposal,0);
+        T2 runningSum=0;
+        for (int j=laneid;j<b_c;j+=WARP_SIZE) {
+            shared_qkt[i][j]-=m_ijProposal;
+            shared_qkt[i][j]=exp(shared_qkt[i][j]);
+            runningSum+=shared_qkt[i][j];
+        }
+        for (int offset=WARP_SIZE/2;offset>0;offset>>=1) {
+            runningSum+=__shfl_down_sync(0xFFFFFFFF,runningSum,offset);//l_{ij} calculation
+        }
+        runningSum=__shfl_sync(0xFFFFFFFF,runningSum,0);
+        T2 curMax=maxValues[i];
+        T2 curRunningSum=sumValues[i];//m_i
+        T2 l_inew=exp(curMax-max(curMax,m_ijProposal))*curRunningSum+exp(m_ijProposal-max(curMax,m_ijProposal))*runningSum;//l_i^{new} 
+        if (laneid == 0) {
+            intermediateRowMaxes[i]=m_ijProposal;
+        }
+        //update O_i
+        for (int j=laneid;j<qkv_dim;j+=WARP_SIZE) {
+            output[i][j]=(curRunningSum/l_inew)*exp(curMax-max(curMax,m_ijProposal))*output[i][j];
+        }
+        sumValues[i]=l_inew;
+        __syncthreads();
+    }
+    //cast qkt to T1
+    for (int i=tid;i<b_r*b_c;i+=WARP_SIZE*WARPS_PER_BLOCK) {
+        casted_qkt[i/b_c][i%b_c]=shared_qkt[i/b_c][i%b_c];
+    }
+    __syncthreads();
+    //handle p_{ij} by v_j multiplication. p_{ij} is in casted_qkt as a b_r x b_c(16x16 tiling). v_j is shared_v as a b_c x qkv_dim (16x8 tiling) 
+    int req_x_tiles=ceil(qkv_dim/TILE_X_SIZE);
+    int req_y_tiles=ceil(b_c/TILE_Y_SIZE);
+    int req_tiles=req_x_tiles*req_y_tiles;
+    for (int i=warpid;i<req_tiles;i+=WARPS_PER_BLOCK) {
+        T2 rC[4]={0,0,0,0};
+        int output_u_left[2]={(i)/req_x_tiles*TILE_Y_SIZE,(i)%req_x_tiles*TILE_X_SIZE};//split output tile work across warps 
+        for (int j=0;j<(b_c/SHARED_Q_K_DIM);j++) {
+            int p_u_left[2]={output_u_left[0],j*SHARED_Q_K_DIM};
+            int v_u_left[2]={j*SHARED_Q_K_DIM,output_u_left[1]};
+            T1 p_elements[8]=[
+                casted_qkt[p_u_left[0]+laneid/4][p_u_left[1]+2*(laneid%4)],
+                casted_qkt[p_u_left[0]+laneid/4][p_u_left[1]+2*(laneid%4)+1],
+                casted_qkt[p_u_left[0]+laneid/4+8][p_u_left[1]+2*(laneid%4)],
+                casted_qkt[p_u_left[0]+laneid/4+8][p_u_left[1]+2*(laneid%4)+1],
+                casted_qkt[p_u_left[0]+laneid/4][p_u_left[1]+8+2*(laneid%4)],
+                casted_qkt[p_u_left[0]+laneid/4][p_u_left[1]+8+2*(laneid%4)+1],
+                casted_qkt[p_u_left[0]+laneid/4+8][p_u_left[1]+8+2*(laneid%4)],
+                casted_qkt[p_u_left[0]+laneid/4+8][p_u_left[1]+8+2*(laneid%4)+1]
+            ];
+            T1 v_elements[4]=[
+                shared_v[v_u_left[0]+2*(laneid%4)][v_u_left[1]+laneid/4],
+                shared_v[v_u_left[0]+2*(laneid%4)+1][v_u_left[1]+laneid/4],
+                shared_v[v_u_left[0]+2*(laneid%4)+8][v_u_left[1]+laneid/4],
+                shared_v[v_u_left[0]+2*(laneid%4)+9][v_u_left[1]+laneid/4]
+            ];
+            //use ptx instruction!
+            asm("mma.sync.aligned.m16n8k16.row.col.f32.f16.f16.f32"//just handling the f32 accum f16 mat A,B pattern for now
+        "{%0,%1,%2,%3}, {%4,%5,%6,%7}, {%8,%9}, {%10,%11,%12,%13};\n"
+        : "=f"(rC[0]), "=f"(rC[1]), "=f"(rC[2]), "=f"(rC[3])
+        : "r"(casted_qkt[0]), "r"(casted_qkt[1]), "r"(casted_qkt[2]), "r"(casted_qkt[3]), "r"(v_elements[0]), "r"(v_elements[1]),
+            "f"(rC[0]), "f"(rC[1]), "f"(rC[2]), "f"(rC[3]));
+        }
+        intermediatePV[output_u_left[0]+laneid/4][output_u_left[1]+2*(laneid%4)]=rC[0];
+        intermediatePV[output_u_left[0]+laneid/4][output_u_left[1]+2*(laneid%4)+1]=rC[1];
+        intermediatePV[output_u_left[0]+laneid/4+8][output_u_left[1]+2*(laneid%4)]=rC[2];
+        intermediatePV[output_u_left[0]+laneid/4+8][output_u_left[1]+2*(laneid%4)+1]=rC[3];
+    }
+    __syncthreads();
+    //final O_i update
+    for (int i=warpid;i<b_r;i+=WARPS_PER_BLOCK) {
+        T2 coefficient=exp(intermediateRowMaxes[i]-maxValues[i])/sumValues[i];
+        for (int j=laneid;j<qkv_dim;j+=WARP_SIZE) {
+            output[i][j]+=coefficient*intermediatePV[i][j];
+        }
+
     }
 
-    
+
+
 }
+
+
+
 //parallelize on heads first
 template<typename T1, typename T2,int qkv_dim, int num_heads>
 __global__ void fa1_fwd(T1* q, T1* k, T1* v, T2* maxValues, T2* sumValues, T2* output,int seq_len)
@@ -78,10 +178,13 @@ __global__ void fa1_fwd(T1* q, T1* k, T1* v, T2* maxValues, T2* sumValues, T2* o
     extern __shared__ T1 shared_q[b_r][qkv_dim];
     extern __shared__ T1 shared_k[b_c][qkv_dim];
     extern __shared__ T1 shared_v[b_c][qkv_dim]; 
-    extern __shared__ T2 shared_maxValues[b_c];
-    extern __shared__ T2 shared_sumValues[b_c];
+    extern __shared__ T2 shared_maxValues[b_r];
+    extern __shared__ T2 shared_sumValues[b_r];
     extern __shared__ T2 shared_output[b_r][qkv_dim];
     extern __shared__ T2 shared_qkt[b_r][b_c];
+    extern __shared__ T2 shared_intermediateRowMaxes[b_r];
+    extern __shared__ T1 casted_qkt[b_r][b_c];
+    extern __shared__ T2 shared_intermediatePV[b_r][qkv_dim];
     int warpid=tid/WARP_SIZE;
     int laneid=tid%WARP_SIZE;
 
@@ -109,9 +212,14 @@ __global__ void fa1_fwd(T1* q, T1* k, T1* v, T2* maxValues, T2* sumValues, T2* o
                     if (k+tid<elementsToLoad) {
                         shared_q[(k+tid)/qkv_dim][(k+tid)%qkv_dim]=q[head_prefix+q_prefix+k+tid];
                     }
+
                 }
             }
+            //load in maxValues, sumValues
+
             __syncthreads();
+            calcQKT<T1,T2,b_c,qkv_dim>(shared_q,shared_k,shared_qkt,seq_len,laneid,warpid);
+            reductionStep<T1,T2,b_c,qkv_dim>(shared_qkt,maxValues,sumValues,shared_output,shared_intermediateRowMaxes,shared_intermediatePV,casted_qkt,warpid,laneid);
         }
     }
 }
