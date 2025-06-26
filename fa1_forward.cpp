216cef89e0e94835c5a9dc2bb85a0af1916240aa
Aadit Juneja <aadit.juneja12@gmail.com>
2025-06-25 18:46:49 -0700

    make cuh and cu consistent, fix stoi in cpp runner

--- fa1_forward.cpp
+++ fa1_forward.cpp
@@ -7,7 +7,7 @@ int main(int argc, char* argv[]) {
     if (argc != 2) {
         std::cerr<<"Usage: "<<argv[0]<<" <sequence_length>"<<std::endl;
     }
-    int seq_len=stoi(argv[1])
+    int seq_len=std::stoi(argv[1])
     fa1_fwd_wrapper(seq_len);
     return 0;   
 }
--- fa1_forward.cuh
+++ fa1_forward.cuh
@@ -23,20 +23,20 @@ struct shared_mem_requirements{
 };
 
 
-template <typename T1, typename T2,int b_c, int b_r, int qkv_dim>
-__device__ void calcQKT(T1* shared_q, T1* shared_k, T2* shared_qkt,int seq_len, int laneid,int warpid);
+template <int qkv_dim>
+__device__ void calcQKT(half* shared_q, half* shared_k, float* shared_qkt,int seq_len, int laneid,int warpid, int b_c, int b_r);
 
 
-template<typename T1, typename T2, int b_c, int b_r>
-__device__ void reductionStep(T2* shared_qkt, T2* maxValues, T2* sumValues,T2* output, T2* intermediateRowMaxes, T2* intermediatePV, T1* casted_qkt, int warpid, int laneid);
+template<int qkv_dim>
+__device__ void reductionStep(float* shared_qkt, float* maxValues, float* sumValues,half* shared_v, float* output, float* intermediateRowMaxes, float* intermediatePV, half* casted_qkt, int warpid, int laneid, int tid, int b_c, int b_r);
 
 
 
-template<typename T1, typename T2,int qkv_dim, int num_heads>
-__global__ void fa1_fwd(T1* q, T1* k, T1* v, T2* maxValues, T2* sumValues, T2* output,int seq_len);
+template<int qkv_dim, int num_heads>
+__global__ void fa1_fwd(half* q, half* k, half* v, float* maxValues, float* sumValues, float* output,int seq_len);
 
 
-__host__ void fa1_fwd_wrapper();
+__host__ void fa1_fwd_wrapper(int seq_len);
 
 
 #endif
