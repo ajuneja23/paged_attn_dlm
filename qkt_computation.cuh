b6df5de60a22d92d2997cad23122323bcff9a54f
Aadit Juneja <aadit.juneja12@gmail.com>
2025-06-28 18:04:36 -0700

    add instrinsics for qkt computation

--- qkt_computation.cuh
+++ qkt_computation.cuh
@@ -1,6 +1,9 @@
 #include <cuda_fp16.h>
 #include <cuda_runtime.h>
+#include <cuda_runtime_api.h>
+#include <device_functions.h>
 #include <iostream>
+#include <math_functions.h>
 #include <mma.h>
 #include <random>
 
