f2792278aa74e8e289a67b1dcd99642600bc80e6
Aadit Juneja <aadit.juneja12@gmail.com>
2025-08-18 19:33:43 -0500

    test

--- runTest.sh
+++ runTest.sh
@@ -1,4 +1,4 @@
 modal shell --gpu a100 \
  --image "nvidia/cuda:12.2.0-devel-ubuntu22.04" \
  --add-python 3.11 \
- -c "git clone https://github.com/ajuneja23/paged_attn_dlm && cd paged_attn_dlm && sh runFA.sh"
+ -c "echo "hi" && git clone https://github.com/ajuneja23/paged_attn_dlm && cd paged_attn_dlm && sh runFA.sh"
