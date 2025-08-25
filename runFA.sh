4554a2c8563e994008516687b226fa69dca3e7ac
Aadit Juneja <aadit.juneja12@gmail.com>
2025-08-25 17:17:33 -0400

    new

--- runFA.sh
+++ runFA.sh
@@ -2,7 +2,5 @@
 apt-get update
 apt-get install cmake -y
 cmake -DBUILD_NAIVE_QKT=ON .
-cmake --build . --target fa1forward
-ls
-cd paged_attn_dlm
-./fa1forward 256
+cmake --build . --target qktRunner
+./qktRunner
