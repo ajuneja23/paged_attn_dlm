c2c10f44b7debd624e842870b3a2e2cbf8a995e1
Aadit Juneja <aadit.juneja12@gmail.com>
2025-09-02 15:04:06 -0400

    switch binary from qkt to reductionstep

--- runFA.sh
+++ runFA.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get update
 apt-get install cmake -y
-cmake -DBUILD_NAIVE_QKT=ON .
-cmake --build . --target qktRunner
-./qktRunner
+cmake -DBUILD_REDUCTION_STEP=ON .
+cmake --build . --target reductionStepRunner
+./reductionStepRunner
